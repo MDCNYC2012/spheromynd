75dcd8effaa2fdfd95d28c38f725122cdd4ff152
Anton Narusberg <antonnarusberg@mackie.(none)>
2012-04-28 18:02:46 -0400

    added ThinkGear

--- src/com/devcampnyc/spheromynd/DashboardActivity.java
+++ src/com/devcampnyc/spheromynd/DashboardActivity.java
@@ -5,19 +5,32 @@ import orbotix.robot.base.Robot;
 import orbotix.robot.base.RobotProvider;
 import orbotix.robot.base.RollCommand;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.Toast;
+
+import com.neurosky.thinkgear.TGDevice;
 
 public class DashboardActivity extends Activity {
 
   public static final int STARTUP_ACTIVITY = 1;
 
+  protected static final String TAG = "MindSphero";
+
   private SeekBar mHeadingControl;
   private SeekBar mSpeedControl;
   private Button mStopButton;
+  
+  TGDevice tgDevice;
+  BluetoothAdapter bluetoothAdapter;
+  final boolean rawEnabled = false;
   
   private Robot mRobot;
   
@@ -29,6 +42,19 @@ public class DashboardActivity extends Activity {
     mHeadingControl = (SeekBar) findViewById(R.id.heading_control);
     mSpeedControl = (SeekBar) findViewById(R.id.speed_control);
     mStopButton = (Button) findViewById(R.id.stop_button);
+    
+    bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    
+    if (bluetoothAdapter == null) {
+      // Alert user that Bluetooth is not available
+      Toast.makeText(this, "Bluetooth not available", Toast.LENGTH_LONG).show();
+      finish();
+      return;
+    } else {
+      /* create the TGDevice */
+      tgDevice = new TGDevice(bluetoothAdapter, handler);
+    }
+    
   }
   
   @Override
@@ -88,5 +114,69 @@ public class DashboardActivity extends Activity {
       updateRobotWithControlValues();
     }
   };
+  
+
+  private final Handler handler = new Handler() {
+    @Override public void handleMessage(Message msg) {
+      switch (msg.what) {
+        case TGDevice.MSG_STATE_CHANGE :
+
+          switch (msg.arg1) {
+            case TGDevice.STATE_IDLE :
+              break;
+            case TGDevice.STATE_CONNECTING :
+              Log.d(TAG, "Connecting...\n");
+              break;
+            case TGDevice.STATE_CONNECTED :
+              Log.d(TAG, "Connected.\n");
+              tgDevice.start();
+              break;
+            case TGDevice.STATE_NOT_FOUND :
+              Log.d(TAG, "Can't find\n");
+              break;
+            case TGDevice.STATE_NOT_PAIRED :
+              Log.d(TAG, "not paired\n");
+              break;
+            case TGDevice.STATE_DISCONNECTED :
+              Log.d(TAG, "Disconnected mang\n");
+          }
+
+          break;
+        case TGDevice.MSG_POOR_SIGNAL :
+          Log.d(TAG, "PoorSignal: " + msg.arg1 + "\n");
+          break;
+        case TGDevice.MSG_RAW_DATA :
+          break;
+        case TGDevice.MSG_HEART_RATE :
+          Log.d(TAG, "Heart rate: " + msg.arg1 + "\n");
+          break;
+        case TGDevice.MSG_ATTENTION :
+          mSpeedControl.setProgress(msg.arg1);
+          Log.d(TAG, "Attention: " + msg.arg1 + "\n");
+          break;
+        case TGDevice.MSG_MEDITATION :
+          Log.d(TAG, "Meditation: " + msg.arg1 + "\n");
+
+          break;
+        case TGDevice.MSG_BLINK :
+          Log.d(TAG, "Blink: " + msg.arg1 + "\n");
+          break;
+        case TGDevice.MSG_RAW_COUNT :
+          break;
+        case TGDevice.MSG_LOW_BATTERY :
+          Toast.makeText(getApplicationContext(), "Low battery!", Toast.LENGTH_SHORT).show();
+          break;
+        case TGDevice.MSG_RAW_MULTI :
+        default :
+          break;
+      }
+    }
+  };
+
+
+  public void connectMindwave(View view) {
+    if (tgDevice.getState() != TGDevice.STATE_CONNECTING && tgDevice.getState() != TGDevice.STATE_CONNECTED)
+      tgDevice.connect(rawEnabled);
+  }
   
 }
