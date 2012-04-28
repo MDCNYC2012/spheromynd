ee0cbaba9b583394a4114b545c2573141b7b197d
Mike Caputo <mcdehuevo@gmail.com>
2012-04-28 17:57:46 -0400

    Translator class

---
+++ src/com/devcampnyc/spheromynd/Translator.java
@@ -0,0 +1,68 @@
+package com.devcampnyc.spheromynd;
+
+import android.os.AsyncTask;
+
+public class Translator {
+	
+	public final class SpheroSettings
+	{
+		private float _heading;
+		private float _speed;
+		
+		public SpheroSettings (float heading, float speed) {
+			_heading = heading;
+			_speed = speed;	
+		}
+		
+		public float getHeading()
+		{
+			return _heading;
+		}
+		
+		public float getSpeed()
+		{
+			return _speed;
+		}
+	}
+	
+	public SpheroSettings getSpheroSettings (int meditation, int attention, 
+			int blink)
+	{
+		float heading = 0;
+		float speed = 0;
+		
+		int base_meditation = 25;
+		int base_attention = 25;
+		int base_blink = 50;
+		
+		if (meditation > base_meditation || attention > base_attention)
+		{
+			speed = (meditation / 200) + (attention / 200);
+		}
+		
+		if (blink > base_blink)
+		{
+			heading = 90;
+		}
+		
+		return new SpheroSettings(heading, speed);
+		
+	}
+	
+	/*
+	private void spheroTalk(String command)
+	{
+		
+	}
+	
+	public void spheroRead(String data)
+	{
+		
+	}
+	
+	private void recordSpheroData(String data)
+	{
+				
+	}
+	*/
+}
